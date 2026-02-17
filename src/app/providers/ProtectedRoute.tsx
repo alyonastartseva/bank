c096ccce9208f7d1780763f52a31900b20c2e5b2
Yurii-Zelenkin <flop_i@mail.ru>
2026-02-17 21:25:51 +0500

    логика отображения: основного контента, layout,базовой маршрутизации и  проверка на публичные/приватные роуты.

---
+++ src/app/providers/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+import { Navigate } from "react-router-dom";
+import { AppRoutes } from "../../shared/config/routes";
+
+interface ProtectedRouteProps {
+  children: ReactNode;
+  isAllowed: boolean;
+}
+
+export const ProtectedRoute = ({
+  children,
+  isAllowed,
+}: ProtectedRouteProps) => {
+  if (!isAllowed) {
+    return <Navigate to={AppRoutes.SIGN_IN} replace />;
+  }
+
+  return <>{children}</>;
+};
---
+++ src/shared/config/routes.ts
@@ -0,0 +1,25 @@
+export enum AppRoutes {
+  // Публичные (без layout)
+  ONBOARDING = "/",
+  SIGN_IN = "/sign-in",
+  SIGN_UP = "/sign-up",
+
+  // Приватные  (Header + BottomNavigation)
+  HOME = "/home",
+  STATISTICS = "/statistics",
+  MY_CARDS = "/my-cards",
+  SETTINGS = "/settings",
+  TRANSACTION_HISTORY = "/transaction-history",
+
+  // Приватные (только Header)
+  PROFILE = "/profile",
+  EDIT_PROFILE = "/edit-profile",
+  ADD_NEW_CARD = "/add-new-card",
+  ALL_CARDS = "/all-cards",
+  SEARCH = "/search",
+  SEND_MONEY = "/send-money",
+  REQUEST_MONEY = "/request-money",
+  LANGUAGE = "/language",
+  CHANGE_PASSWORD = "/change-password",
+  TERMS = "/terms",
+}
