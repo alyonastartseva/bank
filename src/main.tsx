4067108c167f320d5024964d3b7f4f7a29f49e9c
Qw1Qer <artem.ei90@mail.ru>
2026-02-19 01:08:44 +0700

    Init global state(rtk) (#3)
    * Init global state(rtk)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import "./app/styles/globals.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {Provider} from "react-redux";
 
 import App from "./app/App.tsx";
+import {store} from "@/app/store/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+      <Provider store={store}>
     <App />
+      </Provider>
   </StrictMode>
+
 );
+
