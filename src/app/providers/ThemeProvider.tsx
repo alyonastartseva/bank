57b733e6023cb016971176433a2ef295804f3e69
ekrin1 <kottovakatya@mail.ru>
2026-03-02 13:57:00 +0300

    доработала тему + fix

---
+++ src/app/providers/ThemeProvider.tsx
@@ -0,0 +1,32 @@
+import { createContext, useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+export const ThemeContext = createContext<{
+  theme: Theme;
+  toggleTheme: () => void;
+}>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    return (localStorage.getItem("theme") as Theme) || "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
