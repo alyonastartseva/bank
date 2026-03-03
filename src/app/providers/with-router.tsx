310f155407d05f5ebe1fd0e623b3fcfef7f860a8
Qw1Qer <artem.ei90@mail.ru>
2026-03-04 04:56:52 +0700

    fix

--- src/app/providers/with-router.tsx
+++ src/app/providers/with-router.tsx
@@ -12,13 +12,13 @@ import { PageLayout } from "@/widgets/page-layout/ui/PageLayout";
 import OnboardingPage from "@/pages/onboarding/OnbordingPage";
 import SignInPage from "@/pages/sign-in/SignInPage";
 import SignUpPage from "@/pages/sign-up/SignUpPage";
-import HomePage from "@/pages/home/HomePage";
+import HomePage from "@/pages/home/HomePage"
 import StatisticsPage from "@/pages/statistics/StatisticsPage";
 import MyCardsPage from "@/pages/my-cards/MyCardsPage";
 import SettingsPage from "@/pages/settings/SettingsPage";
 import TransactionHistoryPage from "@/pages/transaction-history/TransactionHistoryPage";
 import ProfilePage from "@/pages/profile/ProfilePage";
-import EditProfilePage from "@/pages/edit-profile/EditProfilePage";
+import EditProfilePage from "@/pages/edit-profile/ui/EditProfilePage";
 import AddNewCardPage from "@/pages/add-new-card/AddNewCardPage";
 import SearchPage from "@/pages/search/SearchPage";
 import SendMoneyPage from "@/pages/send-money/SendMoneyPage";
