7c50e606429881abedc14b52c0fad87ef3d01222
Mirums <104374820+Mirums@users.noreply.github.com>
2026-03-19 19:01:51 +0300

    Feat/astb 11 edit profile tests (#22)
    * add tests to editProfilePage.tsx

---
+++ src/shared/test/renderWithProviders.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "@/app/store/store.ts";
+import { MemoryRouter } from "react-router-dom";
+
+export function renderWithProviders(ui: React.ReactElement) {
+  return render(
+    <Provider store={store}>
+      <MemoryRouter>{ui}</MemoryRouter>
+    </Provider>
+  );
+}
