076d01acddddf58212af3caafbb569af6b2b2b10
Crackbrain787 <aleks699177@mail.ru>
2026-03-19 08:34:49 +0300

    feat: сдела интернационализацию на существующих страницах

--- src/widgets/auth-page/AuthPage.tsx
+++ src/widgets/auth-page/AuthPage.tsx
@@ -7,8 +7,10 @@ import style from "./AuthPage.module.css";
 import SignUpForm from "@/features/auth/ui/signUpForm/SignUpForm.tsx";
 import * as React from "react";
 import SignInForm from "@/features/auth/ui/SignInForm/SignInForm.tsx";
+import { useTranslation } from "react-i18next";
 
 const AuthPage = () => {
+  const {t} = useTranslation();
   const [login, setLogin] = useState<User>(initialUser);
   const navigate = useNavigate();
   const location = useLocation().pathname === "/sign-in";
@@ -52,7 +54,7 @@ const AuthPage = () => {
         <button className={style.linkBack} onClick={() => navigate(-1)}>
           <img className={style.arrow} src={arrowBack} alt="" />
         </button>
-        <p className={style.signLabel}>{location ? "Sign In" : "Sign Up"}</p>
+        <p className={style.signLabel}>{location ? t('signIn') : t('signUp')}</p>
         {location ? (
           <SignInForm addLoginInfo={addLoginInfo} login={login} />
         ) : (
