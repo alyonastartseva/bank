bc78b35143721643d9b17b0dbaf3c394517f01a5
Mirums <morkeyc@gmail.com>
2026-03-23 15:26:01 +0300

    hotfix: Сделал виртуальный скролл универсальным для любого компонента, в Confluence подробно опишу принцип использования

---
+++ src/shared/ui/VirtualScroll/VirtualScroll.tsx
@@ -0,0 +1,54 @@
+import type {Transaction} from "@/shared/types/typesReducer.ts";
+import {useState} from "react";
+
+type VirtualScroll<T> = {
+    data: T[],
+    heightOfItem: number,
+    heightOfContainer: number,
+    marginBottom: number,
+    renderItem: (item: T, index: number) => React.ReactNode,
+}
+
+export function VirtualScroll<T>({data, heightOfItem, heightOfContainer, marginBottom, renderItem}: VirtualScroll<T>) {
+    // ===Виртуальный скролл ===
+    const [scrollTop, setScrollTop] = useState(0);
+
+    // ===Введение констант=== (берутся из стилей)
+    const itemHeight = heightOfItem + marginBottom; // Высота конкретного контейнер + gap между ними
+    const containerHeight = heightOfContainer; // Высота всего блока транзакций
+    // =========================
+
+    // Расчёты частей необходимых для вирутального скролла
+    const totalHeight = data.length * itemHeight;
+    const visibleCount = Math.ceil(containerHeight / itemHeight);
+
+    const start = Math.floor(scrollTop / itemHeight);
+    const end = start + visibleCount;
+
+    const visibleTransactions = data.slice(start, end);
+
+    return <div
+        onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
+        style={{
+            height: containerHeight,
+            overflowY: "auto",
+            position: "relative",
+        }}
+    >
+        <div style={{height: totalHeight}}>
+            <div
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    transform: `translateY(${start * itemHeight}px)`,
+                }}
+            >
+                {visibleTransactions.map((item, index) =>
+                        renderItem(item, start + index)
+                    )}
+            </div>
+        </div>
+    </div>
+}
--- src/widgets/transaction-list/TransactionList.tsx
+++ src/widgets/transaction-list/TransactionList.tsx
@@ -3,67 +3,29 @@ import TransactionItem from "@/shared/ui/transactionItem/TransactionItem.tsx";
 import type { Transaction } from "@/shared/types/typesReducer.ts";
 import style from "./TransactionList.module.css";
 import { sellAllTransactions } from "@/app/store/slices/bankSlice.ts";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import {VirtualScroll} from "@/shared/ui/VirtualScroll/VirtualScroll.tsx";
 
 const TransactionList = () => {
   const { t } = useTranslation();
   const transactionList = useAppSelector((state) => state.bank.transactions);
   const dispatch = useAppDispatch();
 
-  // ===Виртуальный скролл ===
-  const [scrollTop, setScrollTop] = useState(0);
-
-  // ===Введение констант=== (берутся из стилей)
-  const itemHeight = 42 + 22; // Высота конкретного контейнер + gap между ними
-  const containerHeight = 400; // Высота всего блока транзакций
-  // =========================
-
-  // Расчёты частей необходимых для вирутального скролла
-  const totalHeight = transactionList.length * itemHeight;
-  const visibleCount = Math.ceil(containerHeight / itemHeight);
-
-  const start = Math.floor(scrollTop / itemHeight);
-  const end = start + visibleCount;
-
-  const visibleTransactions = transactionList.slice(start, end);
-
-  // ===============================
   const checkTransactionList = () => {
     if (!transactionList || !transactionList.length) {
       return <div>{t("transaction.empty")}</div>;
     }
     return (
-      <div
-        onScroll={(e) => setScrollTop(e.currentTarget.scrollTop)}
-        style={{
-          height: containerHeight,
-          overflowY: "auto",
-          position: "relative",
-        }}
-      >
-        <div style={{ height: totalHeight }}>
-          <div
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              right: 0,
-              transform: `translateY(${start * itemHeight}px)`,
-            }}
-          >
-            {visibleTransactions.map((transaction: Transaction) => (
-              <TransactionItem
+        <VirtualScroll data={transactionList} heightOfItem={42} heightOfContainer={400} marginBottom={22} renderItem={(transaction: Transaction) => (
+            <TransactionItem
                 key={transaction.id}
                 icon={transaction.icon}
                 name={transaction.name}
                 category={transaction.category}
                 price={transaction.price}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
+            />
+        )}/>
+
     );
   };
 
