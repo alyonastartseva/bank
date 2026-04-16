28dd3c3a2f02489ce1104151879fb3ec21891046
Igor Chudov <ichudov3@gmail.com>
2026-04-16 12:26:39 +0500

    feat(HomePage): add card component

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,22 @@
+import TransactionList from "@/widgets/transaction-list/TransactionList";
+import { Box } from "@mui/material";
+import styles from "./HomePage.module.css";
+import CardComponent from "@/widgets/card/CardComponent";
+import { cardMock } from "@/widgets/card/cardMock";
+
 const HomePage = () => {
-  return <div></div>;
+  
+  return (
+    <Box className={styles.page}>
+      <Box className={styles.container}>
+        <div className={styles.stack}>
+          
+          <CardComponent card={cardMock}/>
+          <TransactionList />
+        </div>
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
