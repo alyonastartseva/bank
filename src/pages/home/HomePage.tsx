bb91fea3a47e19420a3e7eabacd7658e92b759b3
Igor Chudov <ichudov3@gmail.com>
2026-04-17 09:18:17 +0500

    Feat/astb 28 add home page (#31)
    * feat(HomePage): add card component
    
    * feat: реализовал ActionButtons
    
    * feat: реализовал ActionButtons
    
    * feat: реализовал ProfileHeader и добавил декоративный эллипс
    
    * feat: реализовал логику отображения полного имени
    
    * feat: реализовал логику отображения полного имени

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,30 @@
+import { Box } from "@mui/material";
+import CardComponent from "@/widgets/card/CardComponent";
+import TransactionList from "@/widgets/transaction-list/TransactionList";
+import { ProfileHeader } from "@/widgets/profile-header/ui/ProfileHeader";
+import { cardMock } from "@/widgets/card/cardMock";
+import { BottomNavigation } from "@/widgets/bottom-navigation/ui/BottomNavigation";
+import { ActionButtons } from "@/features/action-buttons/ui/ActionButtons";
+import styles from "./HomePage.module.css";
+
 const HomePage = () => {
-  return <div></div>;
+  
+  return (
+    <Box className={styles.page}>
+      {/* Декоративный эллипс */}
+      <div className={styles.ellipse} />
+      
+      <Box className={styles.container}>
+        <div className={styles.stack}>
+          <ProfileHeader />
+          <CardComponent card={cardMock}/>
+          <ActionButtons />
+          <TransactionList />
+          <BottomNavigation />
+        </div>
+      </Box>
+    </Box>
+  );
 };
 
 export default HomePage;
