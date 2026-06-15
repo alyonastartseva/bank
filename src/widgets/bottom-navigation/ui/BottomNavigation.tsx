e737c73effcbbe5a6b2244278eb20fe2c5bddb00
vlad_filatov <vf140704@gmail.com>
2026-06-15 18:59:56 +0300

    MainLayout адаптирован под десктоп

--- src/widgets/bottom-navigation/ui/BottomNavigation.tsx
+++ src/widgets/bottom-navigation/ui/BottomNavigation.tsx
@@ -1,10 +1,12 @@
 import { NavLink } from "react-router-dom";
 import styles from "./BottomNavigation.module.css";
+import NfcIcon from "@mui/icons-material/Nfc";
 
 import homeIcon from "@/shared/icons/home.svg";
 import myCardsIcon from "@/shared/icons/myCards.svg";
 import statisticsIcon from "@/shared/icons/statistics.svg";
 import settingsIcon from "@/shared/icons/settings.svg";
+import logoutIcon from "@/shared/icons/out.svg";
 
 import homeActiveIcon from "@/shared/icons/homeIsActive.svg";
 import myCardsActiveIcon from "@/shared/icons/myCardsIsActive.svg";
@@ -32,31 +34,48 @@ const navItems = [
     icon: settingsIcon,
     activeIcon: settingsActiveIcon,
   },
+  {
+    path: AppRoutes.ONBOARDING,
+    label: "Log out",
+    icon: logoutIcon,
+    activeIcon: logoutIcon,
+  },
 ];
 
 export function BottomNavigation() {
   return (
     <nav className={styles.bottomNav}>
-      {navItems.map((item) => (
-        <NavLink
-          key={item.path}
-          to={item.path}
-          className={({ isActive }) =>
-            isActive ? `${styles.navItem} ${styles.active}` : styles.navItem
-          }
-        >
-          {({ isActive }) => (
-            <>
-              <img
-                src={isActive ? item.activeIcon : item.icon}
-                alt={item.label}
-                className={styles.icon}
-              />
-              <span className={styles.label}>{item.label}</span>
-            </>
-          )}
-        </NavLink>
-      ))}
+      <NfcIcon
+        sx={{
+          fontSize: 50,
+          '@media (max-width: 720px)': {
+            display: 'none',
+          },
+        }}
+        className={styles.logo}/>
+      <ul className={styles.navList}>
+        {navItems.map((item) => (
+          <li className={styles.navListItem} key={item.path}>
+            <NavLink
+              to={item.path}
+              className={({ isActive }) =>
+                isActive ? `${styles.navItem} ${styles.active}` : styles.navItem
+              }
+            >
+              {({ isActive }) => (
+                <>
+                  <img
+                    src={isActive ? item.activeIcon : item.icon}
+                    alt={item.label}
+                    className={styles.icon}
+                  />
+                  <span className={styles.label}>{item.label}</span>
+                </>
+              )}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
--- src/widgets/main-layout/ui/MainLayout.tsx
+++ src/widgets/main-layout/ui/MainLayout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../../header/ui/Header";
 import { BottomNavigation } from "../../bottom-navigation/ui/BottomNavigation";
+import styles from './MainLayout.module.css'
 
 export const MainLayout = () => {
   return (
-    <div className="main-layout">
+    <div className={styles.layout}>
       <Header />
-      <main className="main-layout__content">
+      <main className={styles.content}>
         <Outlet />
       </main>
       <BottomNavigation />
