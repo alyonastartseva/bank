12c3194af8aed3bc3c436d2b888d740c9254df7b
Vikorikus <Vikorikus@gmail.com>
2026-07-01 17:29:02 +0300

    fix: поправил замечания Алёны в PR

--- src/shared/hooks/useDebounce.ts
+++ src/shared/hooks/useDebounce.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-
-export function useDebounce<T>(value: T, delay: number = 300): T {
+const DEFAULT_DELAY = 300;
+export function useDebounce<T>(value: T, delay: number = DEFAULT_DELAY): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
   useEffect(() => {
