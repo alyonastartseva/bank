4287e2b13e34690e54085126490f987ac34c0199
Olga Babukhina <pruceva@mail.ru>
2026-07-10 22:21:41 +0300

    fix: исправлено отображение сайдбара

--- src/entities/user/api/user-api.ts
+++ src/entities/user/api/user-api.ts
@@ -1,16 +1,16 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import type { User } from '../model/types';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { User } from "../model/types";
 
 export const userApi = createApi({
-  reducerPath: 'userApi',
+  reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: '/account-service/api',
+    baseUrl: "/account-service/api",
     prepareHeaders: (headers) => {
-      headers.set('Content-Type', 'application/json');
+      headers.set("Content-Type", "application/json");
       return headers;
     },
   }),
-  tagTypes: ['User'],
+  tagTypes: ["User"],
   endpoints: (build) => ({
     getUser: build.query<User, number>({
       query: (userId) => `/users/${userId}`,
