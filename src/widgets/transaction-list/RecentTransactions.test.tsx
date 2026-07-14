0382edbe410cf476f79a95a3d741f96e1068f4d3
мидхат <studentbgau1@gmail.com>
2026-07-14 18:12:59 +0300

    создал 2 теста на транзакцию.

---
+++ src/widgets/transaction-list/RecentTransactions.test.tsx
@@ -0,0 +1,100 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect } from "vitest";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import bankSlice, { initialUser } from "../../app/store/slices/bankSlice";
+import { localStorageMiddleware } from "../../app/store/middleware/localStorageMiddleware";
+import { baseApi } from "@/entities/user/api/base-api";
+import type { UserState } from "@/shared/types/typesReducer";
+import apple from "@/shared/icons/apple.svg";
+import spotify from "@/shared/icons/spotify.svg";
+import moneyTransfer from "@/shared/icons/moneyTransfer.svg";
+import { MemoryRouter } from "react-router-dom";
+import { RecentTransactions } from "./RecentTransactions";
+
+const user = localStorage.getItem("bank_user");
+const token = localStorage.getItem("bank_token");
+
+const mockList: UserState["transactions"] = [
+  {
+    id: "1",
+    icon: apple,
+    name: "Яблоко",
+    category: "Развлечение",
+    price: "- 600 руб.",
+  },
+  {
+    id: "2",
+    icon: spotify,
+    name: "Спотифай",
+    category: "Музыка",
+    price: "- 1200 руб.",
+  },
+  {
+    id: "3",
+    icon: moneyTransfer,
+    name: "Денежный перевод",
+    category: "Переводная операция",
+    price: "30000 руб.",
+  },
+  {
+    id: "4",
+    icon: moneyTransfer,
+    name: "Мобайл Легендс",
+    category: "Игры",
+    price: "- 2600 руб.",
+  },
+];
+
+const createMockStore = (preloadedItems: UserState["transactions"] = []) =>
+  configureStore({
+    reducer: {
+      bank: bankSlice,
+      [baseApi.reducerPath]: baseApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(localStorageMiddleware).concat(baseApi.middleware),
+    preloadedState: {
+      bank: {
+        user: user ? JSON.parse(user) : initialUser,
+        token: token || "",
+        showPassword: false,
+        isAuth: false,
+        transactions: preloadedItems,
+      },
+    },
+  });
+
+describe("render transactionList", () => {
+  it("empty list, must written Список транзакций пуст", () => {
+    const store = createMockStore();
+    render(
+      <Provider store={store}>
+        <RecentTransactions />
+      </Provider>
+    );
+    screen.debug();
+    expect(screen.getByText(/список транзакций пуст/i)).toBeInTheDocument();
+  });
+  it("try testing list of 4 items", () => {
+    const store = createMockStore(mockList);
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <RecentTransactions />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    screen.debug();
+
+    expect(screen.getByText(/яблоко/i)).toBeInTheDocument();
+    expect(screen.getByText(/развлечение/i)).toBeInTheDocument();
+    expect(screen.getByText(/спотифай/i)).toBeInTheDocument();
+    expect(screen.getByText(/музыка/i)).toBeInTheDocument();
+    expect(screen.getByText(/денежный перевод/i)).toBeInTheDocument();
+    expect(screen.getByText(/переводная операция/i)).toBeInTheDocument();
+    expect(screen.queryByText(/мобайл легендс/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/игры/i)).not.toBeInTheDocument();
+  });
+});
