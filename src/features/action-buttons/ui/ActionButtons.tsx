0b6f8512743be157bebb1dbc92305e8d023821c8
мидхат <studentbgau1@gmail.com>
2026-07-23 20:38:04 +0300

    доперевёл следующие компоненты: сайдбар, на главной "welcome back", кнопки send, receive, loan, topup, таблица транзакций, на графике месяца

--- src/features/action-buttons/ui/ActionButtons.tsx
+++ src/features/action-buttons/ui/ActionButtons.tsx
@@ -4,16 +4,22 @@ import receiveIcon from "@/shared/icons/receive.svg";
 import loanIcon from "@/shared/icons/loan.svg";
 import topupIcon from "@/shared/icons/topup.svg";
 import styles from "./ActionButtons.module.css";
+import { useTranslation } from "react-i18next";
 
-const actions = [
-  { label: "Send", icon: sendIcon, path: "/send-money" },
-  { label: "Receive", icon: receiveIcon, path: "/request-money" },
-  { label: "Loan", icon: loanIcon, path: "/loan" },
-  { label: "Topup", icon: topupIcon, path: "/topup" },
-];
+const useActions = () => {
+  const { t } = useTranslation();
+  const actions = [
+    { label: t("actions.send"), icon: sendIcon, path: "/send-money" },
+    { label: t("actions.receive"), icon: receiveIcon, path: "/request-money" },
+    { label: t("actions.loan"), icon: loanIcon, path: "/loan" },
+    { label: t("actions.topup"), icon: topupIcon, path: "/topup" },
+  ];
+  return actions;
+};
 
 export const ActionButtons = () => {
   const navigate = useNavigate();
+  const actions = useActions();
 
   return (
     <div className={styles.container}>
