3587b74a17cf0bbc2f584b4f27d1fe735060e1b7
Evgeny Kankin <evgenykankin@gmail.com>
2026-07-31 10:02:16 +0300

    import fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import "@/app/styles/globals.css";
+import "./app/styles/globals.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
