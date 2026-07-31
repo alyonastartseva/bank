cf8a6b41968715f58218648b7926acc0263c9c78
Evgeny Kankin <evgenykankin@gmail.com>
2026-07-31 09:48:47 +0300

    transaction api test add

--- src/entities/user/api/base-api.ts
+++ src/entities/user/api/base-api.ts
@@ -2,13 +2,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
+
   baseQuery: fetchBaseQuery({
-    baseUrl: "/api",
+    baseUrl: import.meta.env.VITE_API_URL || "/api",
+
     prepareHeaders: (headers) => {
       headers.set("Content-Type", "application/json");
       return headers;
     },
   }),
+
   tagTypes: ["User", "Settings", "Account", "Transaction"],
+
   endpoints: () => ({}),
 });
