7b9d0f3f732d07c75e9e90c5799249d4cc79a48e
Evgeny Kankin <evgenykankin@gmail.com>
2026-08-04 15:55:42 +0300

    transaction API integrated in transactio list and recent transaction

--- src/entities/user/api/base-api.ts
+++ src/entities/user/api/base-api.ts
@@ -1,13 +1,26 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { RootState } from "@/app/store/store";
+
+const API_BASE_URL = import.meta.env.DEV
+  ? "/api"
+  : import.meta.env.VITE_API_BASE_URL;
 
 export const baseApi = createApi({
   reducerPath: "api",
 
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "/api",
+    baseUrl: API_BASE_URL,
 
-    prepareHeaders: (headers) => {
-      headers.set("Content-Type", "application/json");
+    prepareHeaders: (headers, { getState }) => {
+      const state = getState() as RootState;
+
+      const token =
+        state.bank.token || localStorage.getItem("bank_token");
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
       return headers;
     },
   }),
