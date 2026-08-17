012dcb9a0a3960b0646af5016b9d96bda1954761
мидхат <studentbgau1@gmail.com>
2026-08-17 18:24:27 +0300

    переключение апи при разработке и продакшене

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "@/app/store/store";
 
-// const API_BASE_URL = import.meta.env.DEV ? "" : import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.DEV ? "" : import.meta.env.VITE_API_BASE_URL;
 
 export const baseApi = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
+    baseUrl: API_BASE_URL,
 
     prepareHeaders: (headers, { getState }) => {
       const state = getState() as RootState;
